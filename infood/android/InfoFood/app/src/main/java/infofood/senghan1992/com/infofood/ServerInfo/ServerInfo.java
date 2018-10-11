60bcd8439fc05388a079ef24b02a0d9146d799d2
변승환 <senghan1992@byeonseunghwan-ui-MacBook-Pro.local>
2018-10-11 14:54:07 +0900

    2018-0=10-11
    디자인과 다음 프로세스가 필요하다 어떻게 하면 좋을지...

--- infood/android/InfoFood/app/src/main/java/infofood/senghan1992/com/infofood/ServerInfo/ServerInfo.java
+++ infood/android/InfoFood/app/src/main/java/infofood/senghan1992/com/infofood/ServerInfo/ServerInfo.java
@@ -1,7 +1,7 @@
 package infofood.senghan1992.com.infofood.ServerInfo;
 
 public interface ServerInfo {
-    public static final String IP = "192.168.1.4";
+    public static final String IP = "192.168.0.15";
     public static final String SERVER_IP = "http://"+ IP+":8080/infood/mobile/";
     public static final String SERVER_IP_PHOTO = "http://"+ IP+":8080/infood/resources/upload/";
 }
